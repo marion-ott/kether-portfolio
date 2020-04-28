ffdf3d9f5b37c1b8b90706078ef6a4fd150939f1
Marion <marion_ott@me.com>
2020-04-28 20:25:11 +0200

    Feature/graphql client (#38)
    * feature(graphql): add queries & mutations client side
    
    * feature(graphql): add queries & mutations client side

--- client/src/pages/Works.js
+++ client/src/pages/Works.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { useQuery } from "@apollo/react-hooks";
+import { GET_WORKS } from "./../graphql/queries/works";
+
 import Card from "../components/organisms/card";
 import Nav from "../components/organisms/nav";
 import { Link } from "react-router-dom";
@@ -8,25 +11,43 @@ import TitleGroup from "../components/molecules/card/TitleGroup";
 
 function CardComp({ name, place, link }) {
   return (
-    <Link style={{ color: Theme.colors.main.secondary, textDecoration: "none" }} to={`/work/${link}`}>
+    <Link
+      style={{ color: Theme.colors.main.secondary, textDecoration: "none" }}
+      to={`/work/${link}`}
+    >
       <Card>
-          <TitleGroup title={name} titleSize="h2" subtitle={place} />
+        <TitleGroup title={name} titleSize="h2" subtitle={place} />
       </Card>
     </Link>
-  )
+  );
 }
 
 function Works() {
+  const { loading, error, data } = useQuery(GET_WORKS);
+
   return (
-      <>
-        <Nav />
-        <section style={{ padding: "150px", minHeight: "100vh", width: "100%", backgroundColor: Theme.colors.main.primary, display: "flex", justifyContent: "space-around" }}>
-          <CardComp name="Tom Clancy’s EliteSquad" place="Ubisoft" link="elite-squad" />
-          <CardComp name="Hungry Sharks" place="Ubisoft" link="hungry-sharks" />
-          <CardComp name="Xenos" place="Hetic" link="xenos" />
-        </section>
-      </>
-  )
+    <>
+      <Nav />
+      <section
+        style={{
+          padding: "150px",
+          minHeight: "100vh",
+          width: "100%",
+          backgroundColor: Theme.colors.main.primary,
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <CardComp
+          name="Tom Clancy’s EliteSquad"
+          place="Ubisoft"
+          link="elite-squad"
+        />
+        <CardComp name="Hungry Sharks" place="Ubisoft" link="hungry-sharks" />
+        <CardComp name="Xenos" place="Hetic" link="xenos" />
+      </section>
+    </>
+  );
 }
 
 export default Works;
