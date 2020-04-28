67466aa0e5859cba59c00bf5629214f9ffbbb018
Marion <marion_ott@me.com>
2020-04-28 20:24:18 +0200

    feature(graphql): add queries & mutations client side

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './pages/App';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import './fonts/spacegrotesk/SpaceGrotesk-Regular.otf'
-import './fonts/spacegrotesk/SpaceGrotesk-SemiBold.otf'
-import './fonts/syne/Syne-Bold.otf'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./pages/App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import "./fonts/spacegrotesk/SpaceGrotesk-Regular.otf";
+import "./fonts/spacegrotesk/SpaceGrotesk-SemiBold.otf";
+import "./fonts/syne/Syne-Bold.otf";
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1',
+  uri: "https://eu1.prisma.sh/marion-36c618/server/dev",
 });
 
 ReactDOM.render(
@@ -18,5 +18,5 @@ ReactDOM.render(
       <App />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
